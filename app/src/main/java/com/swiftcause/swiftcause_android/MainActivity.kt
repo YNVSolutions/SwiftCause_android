b58c9e720245b15f24ad8a2581a217a7fbf33fc1
Jitesh Singh <jiteshSingh4305@gmail.com>
2025-05-29 13:49:35 +0530

    Added Firebase Ui authentication.

--- app/src/main/java/com/swiftcause/swiftcause_android/MainActivity.kt
+++ app/src/main/java/com/swiftcause/swiftcause_android/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.swiftcause.swiftcause_android
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.swiftcause.swiftcause_android.ui.theme.SwiftCause_androidTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +30,48 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SwiftCause_androidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                FirebaseAuthScreen();
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FirebaseAuthScreen(){
+    val context = LocalContext.current;
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val response = IdpResponse.fromResultIntent(result.data)
+        if (result.resultCode == Activity.RESULT_OK){
+            // login successful
+            val user = FirebaseAuth.getInstance().currentUser
+            Log.d("Fire-Auth","the user is , ${user?.email}, ${user?.uid}")
+        }else{
+            Log.d("Fire-Auth", "the login was not successful due to : ${response?.error?.errorCode}")
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        Button(onClick = {
+            val provider = arrayListOf(
+                AuthUI.IdpConfig.GoogleBuilder().build(),
+                AuthUI.IdpConfig.EmailBuilder().build()
+            )
+
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(provider)
+                .setIsSmartLockEnabled(false) // for testing
+                .build()
+
+            launcher.launch(signInIntent)
+        }){
+            Text("Sign in")
+        }
+    }
 }
+
+
 
